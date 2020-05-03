5675bf4842e9d007a9b3b7563e5426687c6d2280
Peter O'Sullivan <peter@peterosullivan.net>
2020-05-03 10:19:39 +1000

    Change to interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,23 @@
 #include "pitches.h"
 #include "secret.h"
 
-
-//#define DETECT 13  // pin D7 for  sensor
-//#define DETECT 15  // pin D8 for  sensor
-#define DETECT 2  // pin D4 for  sensor
-int tonePin = 12;
-
-int count = 0;
-int lazer_previous_state = HIGH;
+const byte interruptPin = D4; // pin 2 =  D4 for  sensor
+const byte tonePin = D6;      //pin 12;
+volatile boolean interrupt_occurred = false;
+volatile int count = 0;
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 //LiquidCrystal_I2C lcd(0x27,20,4);
 
+void ICACHE_RAM_ATTR ISR(){
+  count++;
+  interrupt_occurred = true;   // Record that an interrupt occurred
+}
+
 void setup() {
   Serial.begin(115200);
-  pinMode(DETECT, INPUT);//define detect input pin
+  pinMode(interruptPin, INPUT);                                //define interruptPin input pin
+  attachInterrupt(digitalPinToInterrupt(interruptPin), ISR, FALLING); //Respond to falling edges on the pin
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -42,63 +44,48 @@ void setup() {
   delay(2000);
   lcd.clear();
 
-
   lcd.print("Counter: ");
   lcd.setCursor(0, 1);
   lcd.print(count);
-  //lcd.setCursor(0,2);
-  //lcd.print("Arduino LCM IIC 2004");
-  //lcd.setCursor(2,3);
-  //lcd.print("Power By Ec-yuan!");
-  //lcd.print(WiFi.localIP());
 }
 
-void loop() {
-  int detected = digitalRead(DETECT);// read Laser sensor
- 
-  if(detected == LOW && lazer_previous_state == HIGH){
-    //Serial.println("Beam broken");
-     // Laser Sensor code for Robojax.com
-    //Serial.print("Count Value: ");
-    //Serial.println(count);
-    count++;
-    
-    /*
+void playSound(){
+  if (count == 100){
     //mario_sound
-    tone(tonePin, NOTE_E6,125);
+    tone(tonePin, NOTE_E6, 125);
     delay(130);
-    tone(tonePin, NOTE_G6,125);
+    tone(tonePin, NOTE_G6, 125);
     delay(130);
-    tone(tonePin, NOTE_E7,125);
+    tone(tonePin, NOTE_E7, 125);
     delay(130);
-    tone(tonePin, NOTE_C7,125);
+    tone(tonePin, NOTE_C7, 125);
     delay(130);
-    tone(tonePin, NOTE_D7,125);
+    tone(tonePin, NOTE_D7, 125);
     delay(130);
-    tone(tonePin, NOTE_G7,125);
+    tone(tonePin, NOTE_G7, 125);
     delay(125);
     noTone(tonePin);
-    */
-     
+  }
+  else{
     //simple beep tone
     tone(tonePin, 170);
     delay(50);
     noTone(tonePin);
+  }
+}
 
-    lazer_previous_state = LOW;
+void loop() {
+  
+  if (interrupt_occurred){
+    playSound();
+    interrupt_occurred = false;
   }
-  else if (detected == HIGH && lazer_previous_state == LOW) {
-    lazer_previous_state = HIGH;
-  }
-  else{
-    Serial.print("Current: ");
-    Serial.print(detected);
-    Serial.print(" Pre: ");
-    Serial.print(lazer_previous_state);
-    Serial.print(" Counter: ");
-    Serial.println(count);
-  }
-
+ 
+  Serial.print("Current: ");
+  Serial.print(" Pre: ");
+  Serial.print(" Counter: ");
+  Serial.println(count);
+  
   //delay(200);
   lcd.setCursor(0, 1);
   lcd.print(count);
