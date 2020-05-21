1b76d91166b2c21d35dcabe2af6991120bf7bfe2
Peter O'Sullivan <peter@peterosullivan.net>
2020-05-21 18:05:12 +1000

    Basic webserver with routes

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,37 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include "pitches.h"
 #include "secret.h"
-#include <math.h>
 #include <vector>
+#include <numeric>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 
 const byte interruptPin = D4; // pin 2 =  D4 for  sensor
-const byte tonePin = D6;      //pin 12;
+const byte tonePin =      D6; // pin 12;
+
 volatile boolean interrupt_occurred = false;
+
 volatile int score = 0;
 int target_score = 10;
 int percent_score = 0;
 float average_score = 0.0;
 
-int past_scores [3];
-std::vector<int> ps;
+std::vector<int> past_scores;
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 //LiquidCrystal_I2C lcd(0x27,20,4);
 
-void ICACHE_RAM_ATTR ISR(){
-  score++;
-  interrupt_occurred = true;   // Record that an interrupt occurred
-}
+ESP8266WebServer server(80);
 
 float calculateAverage(){
-  float total = 0.0;
-  for(int i = 0; i < ps.size(); i++){
-    total += ps[i];
-  }
-  return total / ps.size();
-}
-
-void newGame(){
-  score = 0;
-  average_score = calculateAverage();
+  float average = accumulate( past_scores.begin(), past_scores.end(), 0.0)/past_scores.size(); 
+  return average;
 }
 
 void updateDisplay(){
-  percent_score = (score * 100) / 10;
-  Serial.println(percent_score);
-
-  if(score > target_score){
-    score--;
-    percent_score = (score * 100) / 10;
-    ps.push_back(percent_score);
-    newGame();
-    percent_score = 0;
-  }
-
   lcd.clear();
   lcd.print("Score ");
   lcd.print(score);
@@ -62,13 +43,89 @@ void updateDisplay(){
   lcd.setCursor(0, 1);
 
   lcd.print("Game ");
-  lcd.print(ps.size());
+  lcd.print(past_scores.size());
   lcd.print(" AVG ");
   lcd.print(average_score, 1); 
 }
 
+
+void newGame(){
+  //record scores
+  past_scores.push_back(percent_score);
+  average_score = calculateAverage();
+
+  //rest scores
+  percent_score = 0;
+  score = 0;
+  updateDisplay();
+}
+
+
+void handleRoot() {
+  String message = "<h1>The Chipinator</h1>\n\n\n";
+  message += "Current Score: ";
+  message += score;
+  message += "\n\nNumber Games: ";
+  message += past_scores.size();
+  message += "\nAverage Score: ";
+  message += average_score;
+  server.send(200, "text/plain", message);
+}
+
+void newResetRoute(){
+  
+  //clear current and past scores
+  past_scores.clear();
+  percent_score = 0;
+  average_score = 0;
+  score = 0;
+  updateDisplay();
+
+  String message = "Reset!\n\n";
+  message += "\n\nCurrent Score: ";
+  message += score;
+  message += "\n\nNumber Games: ";
+  message += past_scores.size();
+  message += "\nAverage Score: ";
+  message += average_score;
+  server.send(200, "text/plain", message);
+}
+
+void newGameRoute(){
+  newGame(); 
+  String message = "new game started!\n\n";
+  message += "\n\nCurrent Score: ";
+  message += score;
+  message += "\n\nNumber Games: ";
+  message += past_scores.size();
+  message += "\nAverage Score: ";
+  message += average_score;
+  server.send(200, "text/plain", message);
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void ICACHE_RAM_ATTR ISR(){
+  score++;
+  interrupt_occurred = true;   // Record that an interrupt occurred
+}
+
 void setup() {
   Serial.begin(115200);
+
   pinMode(interruptPin, INPUT);                                //define interruptPin input pin
   attachInterrupt(digitalPinToInterrupt(interruptPin), ISR, FALLING); //Respond to falling edges on the pin
 
@@ -94,6 +151,17 @@ void setup() {
   lcd.clear();
 
   updateDisplay();
+
+  server.on("/", handleRoot);
+  server.on("/new", newGameRoute);
+  server.on("/reset", newResetRoute);
+  server.onNotFound(handleNotFound);
+
+  if (MDNS.begin("chip")) {
+    Serial.println("MDNS responder started");
+  }
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void playSound(){
@@ -122,9 +190,12 @@ void playSound(){
 }
 
 void loop() {
-  
+  server.handleClient();
+  MDNS.update();
+
   if (interrupt_occurred){
     interrupt_occurred = false;
+    percent_score = (score * 100) / 10;
     playSound();
     updateDisplay();
   }
@@ -135,7 +206,5 @@ void loop() {
   Serial.print(" target: ");
   Serial.println(target_score);
   */
-
-  
 }
  
